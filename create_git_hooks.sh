77c07d06cbe5d3ae4c509e9703b040318caf7623
Steve Henry <stevehenry13@gmail.com>
2014-10-14 10:07:53 -0400

    link pre-commit if it exists in the project

--- create_git_hooks.sh
+++ create_git_hooks.sh
@@ -5,4 +5,8 @@ ln -sf ~/bin/git_hooks/post-commit .git/hooks/post-commit
 ln -sf ~/bin/git_hooks/post-merge .git/hooks/post-merge
 ln -sf ~/bin/git_hooks/post-rewrite .git/hooks/post-rewrite
 
+if [ -f script/pre-commit.sh ]; then
+  ln -sf ../../script/pre-commit.sh .git/hooks/pre-commit
+fi
+
 rm .git/hooks/*.sample
