bd316fc6676b823c819eecba2b4b5f895ee2f2fa
Steve Henry <stevehenry13@gmail.com>
2015-03-18 22:23:15 -0400

    Use the precommit script for prepush

--- create_git_hooks.sh
+++ create_git_hooks.sh
@@ -6,7 +6,7 @@ ln -sf ~/bin/git_hooks/post-merge .git/hooks/post-merge
 ln -sf ~/bin/git_hooks/post-rewrite .git/hooks/post-rewrite
 
 if [ -f script/pre-commit.sh ]; then
-  ln -sf ../../script/pre-commit.sh .git/hooks/pre-commit
+  ln -sf ../../script/pre-commit.sh .git/hooks/pre-push
 fi
 
 rm .git/hooks/*.sample
