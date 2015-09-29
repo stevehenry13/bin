6013ace6b4f7b9850b580a599344afb28ee78e4d
Steve Henry <stevehenry13@gmail.com>
2015-09-29 06:54:53 -0400

    Get more than just feature branches.

--- branch_author.sh
+++ branch_author.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for branch in `git branch -r | grep feature`;do
-       	echo -e `git show --format="%an" $branch | head -n 1` \\t$branch
+for branch in `git branch -r | grep -v -e release -e beta -e develop -e master | sed -n '1!p'`;do
+  echo -e `git show --format="%an" $branch | head -n 1` \\t$branch
 done | sort | column -ts $'\t'
