6652bd9c44ab4f73313c2fd1924e0d150d9a7916
Steve Henry <stevehenry13@gmail.com>
2015-11-18 12:15:10 -0500

    Add last update time to branch table

--- branch_author.sh
+++ branch_author.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for branch in `git branch -r | grep -v -e release -e beta -e develop -e master | sed -n '1!p'`;do
-  echo -e `git show --format="%an" $branch | head -n 1` \\t$branch
+  echo -e `git show --format="%an\t%ar" $branch | head -n 1` \\t$branch
 done | sort | column -ts $'\t'
