d015cd4c3cbe0fc519c039f63e61efc0502ebf44
Steve Henry <stevehenry13@gmail.com>
2018-04-20 21:00:49 -0400

    Add prepare commit message hook

--- create_git_hooks.sh
+++ create_git_hooks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ln -sf ~/bin/git_hooks/post-checkout .git/hooks/post-checkout
-ln -sf ~/bin/git_hooks/post-commit .git/hooks/post-commit
 ln -sf ~/bin/git_hooks/post-merge .git/hooks/post-merge
 ln -sf ~/bin/git_hooks/post-rewrite .git/hooks/post-rewrite
+ln -sf ~/bin/git_hooks/prepare-commit-msg .git/hooks/prepare-commit-msg
 
 if [ -f script/pre-commit.sh ]; then
   ln -sf ../../script/pre-commit.sh .git/hooks/pre-push
